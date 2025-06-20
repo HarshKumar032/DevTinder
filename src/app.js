7955e731e6c79408bbf26a470a1f3ee11e24dcc3
Harsh <harshkumarshaw09@gamil.com>
2025-06-21 01:34:03 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express=require("express");
 const app=express();
 
-app.use("/hellos",(req,res)=>{
-    res.send("Hello from the server!");
+app.get("/user",(req,res)=>{
+    res.send({"firstname":"harsh","lastname":"Shaw"});
 })
 
+app.post("/user",(req,res)=>{
+    res.send("DB updated succesfully");
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Data deleted succesfully");
+})
+
+//This app.use() will match all http call methods to /test
 app.use("/test",(req,res)=>{
     res.send("Just Testing");
 })
