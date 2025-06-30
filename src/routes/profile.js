1225cd37b871b6f5f8f4e85022c55fa3d9948a56
Harsh <harshkumarshaw09@gamil.com>
2025-06-30 21:06:13 +0530

    Created forgot password API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,5 +1,6 @@
 const { userauth } = require("../middlewares/auth.js");
 const { validateeditprofiledata } = require("../utils/validation.js");
+const bcrypt = require("bcrypt");
 
 const express = require("express");
 const profileRouter = express.Router();
@@ -33,6 +34,35 @@ profileRouter.patch("/profile/edit", userauth, async (req, res) => {
     res.send("Profile updated successfully.");
   } catch (error) {
     res.status(400).send(error.message);
+  }
+});
+
+//API for forgot password
+profileRouter.patch("/profile/password", userauth, async (req, res) => {
+  try {
+    const { oldpassword, newpassword } = req.body;
+
+    if (!oldpassword || !newpassword) {
+      return res.status(400).send("Both old and new passwords are required.");
+    }
+
+    const person = req.user;
+
+    // Check if old password matches
+    const isMatch = await bcrypt.compare(oldpassword, person.password);
+    if (!isMatch) {
+      return res.status(401).send("Old password is incorrect.");
+    }
+
+    // Hash and update new password
+    const newhashedpassword = await bcrypt.hash(newpassword, 10);
+    person.password = newhashedpassword;
+    await person.save();
+
+    res.send("Password changed successfully.");
+  } catch (error) {
+    console.error("Error updating password:", error);
+    res.status(500).send("Something went wrong.");
   }
 });
 
